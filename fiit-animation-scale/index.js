aa3d1b696339b44644c9833ad5fc628b167aba98
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2017-11-02 15:46:51 +0800

    Updated styles and animation

--- fiit-animation-scale/index.js
+++ fiit-animation-scale/index.js
@@ -1,4 +1,27 @@
 $(document).ready(() => {
-  
+  var slide = $('.slider-content .slider-single');
+  var slideTotal = slide.length - 1;
+  var slideCurrent = -1;
+
+  function slideInitial() {
+    slideRight();
+    
+    setInterval(function() {
+      slideRight();
+    }, 2000);
+  }
+
+  function slideRight() {
+    if (slideCurrent < slideTotal) {
+      slideCurrent++;
+    } else {
+      slideCurrent = 0;
+    } 
+
+    slide.removeClass('animate-1')
+    slide.eq(slideCurrent).addClass('animate-1')
+  }
+
+  slideInitial()
 })
 
